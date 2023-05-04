7cc2c45cfb532d5402f577452437680bdca6b4ee
Carlos Ramos <carlosramosdev@protonmail.com>
2023-05-04 21:39:49 +0200

    added & implemented getCarById

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
 
+    private cars: string[] = ["Toyota", "Honda", "Jeep"];
+
     @Get()
     getAllCars(){
-        return ["Toyota", "Honda", "Jeep"];
+        return this.cars;
     }
 
+    @Get(":id")
+    getCarById( @Param("id") id: number): string{        
+        return  this.cars[id];
+    }
 }
