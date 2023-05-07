72c09d2babc1a5bf6cc175d1a45b4d2cf2e8fdc4
Carlos Ramos <carlosramosdev@protonmail.com>
2023-05-07 19:59:14 +0200

    CreateCarDto validation pipe created & implemented

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,4 +1,10 @@
+import { IsString } from "class-validator";
+
 export class CreateCarDto {
+
+    @IsString({ message: `La marca debe de ser una cadena de texto`})
     readonly brand: string;
+
+    @IsString({ message: `El modelo debe de ser una cadena de texto`})
     readonly model: string;
 }
